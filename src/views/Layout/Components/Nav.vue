5f4ae07ebc7909d9cfd1e53e9a144ae5e787b596
”yfzan” <yfzan@clustertech.com>
2020-10-17 21:33:20 +0800

    浏览器刷新后菜单高亮设置

--- src/views/Layout/Components/Nav.vue
+++ src/views/Layout/Components/Nav.vue
@@ -2,7 +2,7 @@
   <div id="nav-wrap">
     <h1 class="logo"><img src="../../../assets/logo.png" alt=""></h1>
     <el-menu
-      default-active="1-4-1"
+      :default-active="$route.path"
       class="el-menu-vertical-demo"
       :collapse="isCollapse"
       background-color="transparent"
